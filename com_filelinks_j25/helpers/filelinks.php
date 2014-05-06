950315fe1f6d84a13c723e66a01f3c06bac15140
heleneross <heleneross@gmail.com>
2014-05-06 10:00:58 +0200

    upload changes

--- com_filelinks_j25/helpers/filelinks.php
+++ com_filelinks_j25/helpers/filelinks.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @version     1.0.4
+ * @version     2.0.0
  * @package     com_filelinks
  * @copyright   Copyright (C) Helen 2014. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -30,113 +30,147 @@ class FilelinksHelper
 			"index.php?option=com_categories&extension=com_filelinks.files",
 			$vName == 'categories.files'
 		);
-    //new empty view findfiles
-    JSubMenuHelper::addEntry(
+		//new empty view findfiles
+		JSubMenuHelper::addEntry(
 			JText::_('COM_FILELINKS_TITLE_FINDFILES'),
 			'index.php?option=com_filelinks&view=findfiles',
 			$vName == 'findfiles'
 		);
-    JSubMenuHelper::addEntry(
+		JSubMenuHelper::addEntry(
 			JText::_('Help'),
 			'index.php?option=com_filelinks&view=help',
 			$vName == 'help'
 		);
-		
-if ($vName=='categories.files.catid') {			
-JToolBarHelper::title('FileLinks: Categories (Files)');		
-}
+
+		if ($vName == 'categories.files.catid')
+		{
+			JToolBarHelper::title('FileLinks: Categories (Files)');
+		}
 	}
 
 	/**
 	 * Gets a list of the actions that can be performed.
 	 *
-	 * @return	JObject
-	 * @since	1.6
+	 * @return    JObject
+	 * @since    1.6
 	 */
 	public static function getActions()
 	{
-		$user	= JFactory::getUser();
-		$result	= new JObject;
+		$user = JFactory::getUser();
+		$result = new JObject;
 
 		$assetName = 'com_filelinks';
 
 		$actions = array(
-			'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.edit.own', 'core.edit.state', 'core.delete'
+			'core.admin', 'core.manage', 'core.create', 'core.edit', 'core.edit.own', 'core.edit.state', 'core.delete', 'core.view'
 		);
 
-		foreach ($actions as $action) {
+		foreach ($actions as $action)
+		{
 			$result->set($action, $user->authorise($action, $assetName));
 		}
 
 		return $result;
 	}
-  
-  public static function getDoctypes()
-  {
-      $doctypes = trim(JComponentHelper::getParams('com_filelinks')->get('doctypes'),' ,');
-      $allowedExtensions = strtolower($doctypes) . ',' . strtoupper($doctypes);
-      $allowedExtensions = explode(',', $allowedExtensions);
-      $allowedExtensions = array_map('trim',$allowedExtensions);
-      return $doctypes = "^.*\.(" . implode('|', $allowedExtensions) . ")$";
-  }
-  
-  /*
- * @param string $file Filepath
- * @param int $digits Digits to display
- * @return string|bool Size (KB, MB, GB, TB) or boolean
- */
 
-  public static function getFilesize($file,$digits = 2) 
-  {
-       if (is_file($file)) {
-               $filePath = $file;
-               if (!realpath($filePath)) {
-                       $filePath = $_SERVER["DOCUMENT_ROOT"].$filePath;
-       }
-           $fileSize = filesize($filePath);
-               $sizes = array("TB","GB","MB","KB","B");
-               $total = count($sizes);
-               while ($total-- && $fileSize > 1024) {
-                       $fileSize /= 1024;
-                       }
-               return round($fileSize, $digits)."&nbsp;".$sizes[$total];
-       }
-       return false;
-  }
-  
-  // upload file
-  public static function upload ($filestore, $folder, $filearray, $overwrite = false)
-  {
-     jimport('joomla.filesystem.file');
+	public static function getDoctypes()
+	{
+		$doctypes = trim(JComponentHelper::getParams('com_filelinks')->get('doctypes'), ' ,');
+		$allowedExtensions = strtolower($doctypes) . ',' . strtoupper($doctypes);
+		$allowedExtensions = explode(',', $allowedExtensions);
+		$allowedExtensions = array_map('trim', $allowedExtensions);
+		return $doctypes = "^.*\.(" . implode('|', $allowedExtensions) . ")$";
+	}
 
-     //Clean up filename to get rid of strange characters like spaces etc
-     $filename = str_replace(' ','-',JFile::makeSafe($filearray['name']));
-     
-     //Set up the source and destination of the file
-     $src = $filearray['tmp_name'];
-     if ($folder == $filestore)
-     {
-        $dest = JPATH_ROOT . DIRECTORY_SEPARATOR . $filestore . DIRECTORY_SEPARATOR . $filename;
-     }
-     else
-     {
-        $dest = JPATH_ROOT . DIRECTORY_SEPARATOR . $filestore . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR . $filename;
-     }
+	/*
+	 * @param string $file Filepath
+	 * @param int $digits Digits to display
+	 * @return string|bool Size (KB, MB, GB, TB) or boolean
+	 */
 
-      // don't overwrite
-      if (!$overwrite && JFile::exists($dest))
-      {
-          //echo '<script type="text/javascript">alert("'.$filename.' already exists!");</script>';
-          return array($filename.' already exists!','warning');
-      }
-      
-      // try saving file
-      if (!JFile::upload($src, $dest))
-      {
-        //error saving
-        //echo '<script type="text/javascript">alert("Error saving file");</script>';
-        return array('Error saving file: '. $filename,'error');
-      } 
-     return array('File saved: '. $filename,'message');
-  }
+	public static function getFilesize($file, $digits = 2)
+	{
+		if (is_file($file))
+		{
+			$filePath = $file;
+			if (!realpath($filePath))
+			{
+				$filePath = $_SERVER["DOCUMENT_ROOT"] . $filePath;
+			}
+			$fileSize = filesize($filePath);
+			$sizes = array("TB", "GB", "MB", "KB", "B");
+			$total = count($sizes);
+			while ($total-- && $fileSize > 1024)
+			{
+				$fileSize /= 1024;
+			}
+			return round($fileSize, $digits) . "&nbsp;" . $sizes[$total];
+		}
+		return false;
+	}
+
+	// upload file
+	public static function upload($filestore, $folder, $filearray, $overwrite = false)
+	{
+		jimport('joomla.filesystem.file');
+		$file_error = array(
+			0 => 'There is no error, the file uploaded with success',
+			1 => 'The file is too large',
+			2 => 'The file is too large',
+			3 => 'The file was only partially uploaded',
+			4 => 'No file was uploaded',
+			6 => 'Missing a temporary folder'
+		);
+
+		if ($filearray['error'] > 0)
+		{
+			return array('File error: ' . $filearray['error'] . ' - ' . $file_error[$filearray['error']], 'error');
+		}
+
+		//Clean up filename to get rid of strange characters like spaces etc
+		$filename = str_replace(' ', '-', JFile::makeSafe($filearray['name']));
+
+		// is there only one . (period) in the filename
+		if (substr_count($filename, '.') != 1)
+		{
+			return array('Invalid filename: ' . $filename, 'error');
+		}
+
+		//check it is a valid filetype again
+		$doctypes = '@' . self::getDoctypes() . '@';
+
+		if (preg_match($doctypes, $filename))
+		{
+			//Set up the source and destination of the file
+			$src = $filearray['tmp_name'];
+			if ($folder == $filestore)
+			{
+				$dest = JPATH_ROOT . DIRECTORY_SEPARATOR . $filestore . DIRECTORY_SEPARATOR . $filename;
+			}
+			else
+			{
+				$dest = JPATH_ROOT . DIRECTORY_SEPARATOR . $filestore . DIRECTORY_SEPARATOR . $folder . DIRECTORY_SEPARATOR . $filename;
+			}
+
+			// don't overwrite
+			if (!$overwrite && JFile::exists($dest))
+			{
+				return array($filename . ' already exists!', 'warning');
+			}
+
+			// try saving file
+			if (!JFile::upload($src, $dest))
+			{
+				//error saving
+				return array('Error saving file: ' . $filename, 'error');
+			}
+
+			chmod($dest, 0644);
+			return array('File saved: ' . $filename, 'message');
+		}
+		else
+		{
+			return array('Invalid filetype: ' . $filename, 'error');
+		}
+	}
 }
