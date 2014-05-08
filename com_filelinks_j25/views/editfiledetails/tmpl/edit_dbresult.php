a99ecc06a68b52f8260e5b98bbf40fbc1af4b025
heleneross <heleneross@gmail.com>
2014-05-08 11:07:03 +0200

    get published state

--- com_filelinks_j25/views/editfiledetails/tmpl/edit_dbresult.php
+++ com_filelinks_j25/views/editfiledetails/tmpl/edit_dbresult.php
@@ -9,6 +9,8 @@
 // no direct access
 defined('_JEXEC') or die;
 
+$pub = array(-2=>'trashed',-1=>'unpublished',0=>'unpublished',1=>'published',2=>'archived');
+
 if ($this->searchdb == 3)
 {
 	$r_tablesnames = 'all';
@@ -28,15 +30,15 @@ $found = false;
 
 if ($this->searchdb > 0)
 {
-	if ($articles = EditFileDetailsHelper::getFilelistArticles($id,$this->filelink))
+	if ($articles = EditFileDetailsHelper::getFilelistArticles($id, $this->filelink))
 	{
-		echo '<h2>Articles</h2><table style="width:100%"><thead><tr><th>id</th><th>title</th><th>published</th></tr></thead><tbody>';
+		echo '<h2 style="margin-bottom:0">Articles</h2><table style="width:100%"><thead><tr class="row1"><th>id</th><th>title</th><th class="fltrt">published</th></tr></thead><tbody>';
 		foreach ($articles as $article)
 		{
 			echo '<tr>';
 			echo '<td>' . $article['id'] . '</td>';
 			echo '<td>' . $article['title'] . '</td>';
-			echo '<td>' . $article['state'] . '</td>';
+			echo '<td class="fltrt">' . $pub[$article['state']] . '</td>';
 			echo '</tr>';
 		}
 		echo '</tbody></table>';
@@ -45,7 +47,7 @@ if ($this->searchdb > 0)
 
 	if ($modules = EditFileDetailsHelper::getFilelistModules($id, $this->filelink))
 	{
-		echo '<h2>Modules</h2><table style="width:100%"><thead><tr><th>id</th><th>title</th><th>module</th><th>position</th><th>published</th></tr></thead><tbody>';
+		echo '<h2 style="margin-bottom:0">Modules</h2><table style="width:100%"><thead><tr class="row1"><th>id</th><th>title</th><th>module</th><th>position</th><th class="fltrt">published</th></tr></thead><tbody>';
 		foreach ($modules as $module)
 		{
 			echo '<tr>';
@@ -53,7 +55,7 @@ if ($this->searchdb > 0)
 			echo '<td>' . $module['title'] . '</td>';
 			echo '<td>' . $module['module'] . '</td>';
 			echo '<td>' . $module['position'] . '</td>';
-			echo '<td>' . $module['published'] . '</td>';
+			echo '<td class="fltrt">' . $pub[$module['published']] . '</td>';
 			echo '</tr>';
 		}
 		echo '</tbody></table>';
@@ -62,20 +64,28 @@ if ($this->searchdb > 0)
 
 	if (!empty($r_tablesnames) && $others = EditFileDetailsHelper::showTables($id, $r_tablesnames, $this->filelink))
 	{
-		echo '<h2>Other</h2><table style="width:100%"><thead><tr><th>id</th><th>title</th><th>db table</th></tr></thead><tbody>';
+		echo '<h2 style="margin-bottom:0">Other</h2><table style="width:100%"><thead><tr class="row1"><th>id</th><th>title</th><th>db table</th><th class="fltrt">published</th></tr></thead><tbody>';
 		foreach ($others as $tables => $other)
 		{
 			foreach ($other as $key => $value)
 			{
+				$status = '';
+				$status = isset($value['status']) ? $value['status'] : $status;
+				$status = isset($value['state']) ? $value['state'] : $status;
+				$status = isset($value['published'])? $value['published'] : $status;
 				echo '<tr>';
 				echo '<td>' . $key . '</td>';
 				echo '<td>' . $value['title'] . '</td>';
 				echo '<td>' . $tables . '</td>';
+				echo '<td class="fltrt">' . (empty($status) ? 'unknown' : $pub[$status]) . '</td>';
 				echo '</tr>';
 			}
 		}
 		echo '</tbody></table>';
 		$found = true;
 	}
-	if (!$found) {echo $foundtext;}
+	if (!$found)
+	{
+		echo $foundtext;
+	}
 }
