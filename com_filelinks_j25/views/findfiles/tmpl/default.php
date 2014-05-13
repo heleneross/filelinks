31cd387a6d7ee3d8ca70d5b9bfa0e7ec2a227dc8
heleneross <heleneross@gmail.com>
2014-05-13 17:52:53 +0200

    excluded folders, permission checks, new flag
    ability to exclude certain folders from being shown in dropdowns, file
    list etc. Added a new flag to the last added file so you can find it
    easily, persists with cookies until session end.
    only show upload form to users with sufficient permissions. Fixed
    filelinksfolder cookie, should be value not text.

--- com_filelinks_j25/views/findfiles/tmpl/default.php
+++ com_filelinks_j25/views/findfiles/tmpl/default.php
@@ -8,19 +8,24 @@
  */
 // no direct access
 defined('_JEXEC') or die;
-
+$user = JFactory::getUser();
 // root of documents folder
 $filestore = JComponentHelper::getParams('com_filelinks')->get('filestore');
+$showuser = JComponentHelper::getParams('com_filelinks')->get('showuser');
+$excluded = trim(JComponentHelper::getParams('com_filelinks')->get('excluded'), ', ');
+$excluded_folders = explode(',', $excluded);
+$excluded_folders = array_map('trim', $excluded_folders, array_fill(0, count($excluded_folders), '/ '));
 $filestore = trim($filestore, '.\\/ ');
 //allowed document types - from helper
 $doctypes = FilelinksHelper::getDoctypes();
 $jregex = '/' . $doctypes . '/';
+$ds = DIRECTORY_SEPARATOR;
 ?>
 	<script type="text/javascript">
 		Joomla.submitbutton = function (pressbutton) {
 			var form = document.getElementById('folderform');
 			var foldercookie = document.getElementsByName('folders')[0];
-			document.cookie = 'filelinksfolder=' + foldercookie.options[foldercookie.selectedIndex].text;
+			document.cookie = 'filelinksfolder=' + foldercookie.options[foldercookie.selectedIndex].value;
 			form.submit();
 		};
 		Joomla.submitbutton_u = function (pressbutton) {
@@ -58,21 +63,27 @@ if ($task == 'uploadfile')
 $cookie = $input->cookie->get('filelinksfolder', '', 'cmd');
 $folders = ($cookie) ? $cookie : $input->getCmd('folders', '');
 
-
 $list = JFolder::folders(JPATH_ROOT . '/' . $filestore, '.', false, false);
+$list = array_diff($list, $excluded_folders);
 array_unshift($list, $filestore);
+$userarray = array();
+
 echo '<div class="findfiles">';
 echo '<form enctype="multipart/form-data" action="' . JRoute::_('index.php?option=com_filelinks&view=findfiles') . '" method="post" name="folderform" id="folderform">';
 echo '<label for="folders">Folder: </label><select name="folders" id="folders">';
 foreach ($list as $item)
 {
+	if (is_numeric($item))
+	{
+		$userarray[$item] = JFactory::getUser($item)->name;
+	}
 	if ($item == $folders)
 	{
-		echo '<option value="' . $item . '" selected>' . $item . '</option>';
+		echo '<option value="' . $item . '" selected>' . $item . (isset($userarray[$item]) && $showuser ? ' : ' . $userarray[$item] : '') . '</option>';
 	}
 	else
 	{
-		echo '<option value="' . $item . '">' . $item . '</option>';
+		echo '<option value="' . $item . '">' . $item . (isset($userarray[$item]) && $showuser ? ' : ' . $userarray[$item] : '') . '</option>';
 	}
 }
 ?>
@@ -81,6 +92,9 @@ foreach ($list as $item)
 	<input type="hidden" name="task" value="folder"/>
 <?php echo JHtml::_('form.token'); ?>
 	</form>
+
+
+<?php if ($user->authorise('core.create', 'com_filelinks')) : ?>
 	<form enctype="multipart/form-data"
 	      action="<?php echo JRoute::_('index.php?option=com_filelinks&view=findfiles') ?>" method="post"
 	      name="fileupload" id="fileupload">
@@ -88,15 +102,36 @@ foreach ($list as $item)
 		<input type="checkbox" id="overwrite" name="overwrite" value="true">
 		<label for="chosen">Upload file: </label>
 		<input class="input_box" id="chosen" name="chosen" type="file" size="80">
+		<?php
+		if ($folders == $filestore)
+		{
+			$subfolders = JFolder::listFolderTree(JPATH_ROOT . '/' . $filestore, '.', 1);
+		}
+		else
+		{
+			$subfolders = JFolder::listFolderTree(JPATH_ROOT . '/' . $filestore . '/' . $folders, '.');
+		}
+
+		echo '<label for="subfolders">Folder: </label><select name="subfolders" id="subfolders">';
+		echo '<option value="' . base64_encode($folders) . '">' . $folders . (isset($userarray[$folders]) && $showuser ? ' : ' . $userarray[$folders] : '') . '</option>';
+		foreach ($subfolders as $subfolder)
+		{
+			$displayname = substr(str_replace($ds, '/', $subfolder['relname']), strlen($filestore) + 2);
+			if (!in_array($displayname, $excluded_folders))
+			{
+				echo '<option value="' . base64_encode($displayname) . '">' . $displayname . (isset($userarray[$displayname]) && $showuser ? ' : ' . $userarray[$displayname] : '') . '</option>';
+			}
+		}
+		echo '</select>';
+		?>
 		<input class="button" type="button" value="Submit" onclick="Joomla.submitbutton_u()"/>
 		<input type="hidden" name="task" value="uploadfile"/>
 		<input type="hidden" name="folder" value="<?php echo $folders ?>">
 		<?php echo JHtml::_('form.token'); ?>
 	</form>
+<?php endif; ?>
 <?php
-$ds = DIRECTORY_SEPARATOR;
 $site = str_replace($ds, '/', JPATH_SITE);
-
 
 if ($filestore == '')
 {
@@ -106,10 +141,16 @@ if ($filestore == '')
 }
 else
 {
-	if ($task == 'uploadfile')
+	if ($task == 'uploadfile' && $user->authorise('core.create', 'com_filelinks'))
 	{
-		$message = FilelinksHelper::upload($filestore, $input->getCmd('folder', ''), $input->files->get('chosen'), $input->getBool('overwrite', false));
+		$message = FilelinksHelper::upload($filestore, $input->getCmd('subfolders', ''), $input->files->get('chosen'), $input->getBool('overwrite', false));
 		JFactory::getApplication()->enqueueMessage($message[0], $message[1]);
+		if ($message[1] == 'message')
+		{
+			$uploaded_file = $message[2];
+			setcookie("uploaded_file", $uploaded_file);
+		}
+
 	}
 	if (($task == 'folder' || $cookie) && $folders && $folders != $filestore)
 	{
@@ -121,6 +162,7 @@ else
 		$folders = '';
 		$files = JFolder::files(JPATH_ROOT . '/' . $filestore, $doctypes, false, true);
 	}
+
 	$urlarray = array();
 	foreach ($this->items as $url)
 	{
@@ -149,34 +191,46 @@ else
 	</thead>
 	<tbody>
 	<?php
-	$replace = array($ds, $site . '/' . $filestore . $folders . '/');
+	$uploaded_file = isset($uploaded_file) ? $uploaded_file : $input->cookie->get('uploaded_file', '','path');
+	$replace = array($ds, $site . '/' . $filestore);
+	$files = str_replace($replace, '/', $files);
+	$files = array_map('ltrim', $files, array_fill(0, count($files), '/'));
 	foreach ($files as $file)
 	{
-		$file = ltrim(str_replace($replace, '/', $file), '/');
-		$filetype = strtolower(JFile::getExt($file));
-		echo '<tr>';
-		echo '<td><a href="' . JURI::root() . $filestore . $folders . '/' . $file . '" target="_blank" class="view">[view]</a></td>';
-		if (array_key_exists($filestore . $folders . '/' . $file, $urlarray))
+		if (in_array(dirname($file), $excluded_folders))
 		{
-			echo '<td><a href="' . JRoute::_('index.php?option=com_filelinks&task=editfiledetails.edit&id=' . $urlarray[$filestore . $folders . '/' . $file][1]) . '" class="edit">[edit]</a></td>';
-			echo '<td><i class="' . $filetype . '"></i></td>';
-			echo '<td><i class="state' . $urlarray[$filestore . $folders . '/' . $file][0] . '"></i></td>';
-			echo '<td><span class="pub' . $urlarray[$filestore . $folders . '/' . $file][0] . '">' . $filestore . $folders . '/' . $file . '</span></td>';
-			echo '<td><span class="pub' . $urlarray[$filestore . $folders . '/' . $file][0] . '">' . $urlarray[$filestore . $folders . '/' . $file][2] . '</span></td>';
+			continue;
+		}
+		if($filestore . '/' . $file == $uploaded_file)
+		{
+			$new = '<i class="new"></i>';
 		}
 		else
 		{
-			echo '<td><a href="' . JRoute::_('index.php?option=com_filelinks&view=editfiledetails&layout=edit&task=add&title=' . base64_encode($file) . '&url=' . base64_encode($filestore . $folders . '/' . $file)) . '" class="add">[add]</a></td>';
+			$new = '';
+		}
+		$filetype = strtolower(JFile::getExt($file));
+		echo '<tr>';
+		echo '<td><a href="' . JURI::root() . $filestore . '/' . $file . '" target="_blank" class="view">[view]</a></td>';
+		if (array_key_exists($filestore . '/' . $file, $urlarray))
+		{
+			echo '<td><a href="' . JRoute::_('index.php?option=com_filelinks&task=editfiledetails.edit&id=' . $urlarray[$filestore . '/' . $file][1]) . '" class="edit">[edit]</a></td>';
+			echo '<td><i class="' . $filetype . '"></i></td>';
+			echo '<td><i class="state' . $urlarray[$filestore . '/' . $file][0] . '"></i></td>';
+			echo '<td><span class="pub' . $urlarray[$filestore . '/' . $file][0] . '">' . $filestore . '/' . $file . '</span>' . $new . '</td>';
+			echo '<td><span class="pub' . $urlarray[$filestore . '/' . $file][0] . '">' . $urlarray[$filestore . '/' . $file][2] . '</span></td>';
+		}
+		else
+		{
+			echo '<td><a href="' . JRoute::_('index.php?option=com_filelinks&view=editfiledetails&layout=edit&task=add&title=' . base64_encode($file) . '&url=' . base64_encode($filestore . '/' . $file)) . '" class="add">[add]</a></td>';
 			echo '<td><i class="' . $filetype . '"></i></td>';
 			echo '<td><i class="stateu"></i></td>';
-			echo '<td><a class="copylink" href="#" onclick="window.prompt(' . "'Press CTRL+C, then ENTER'" . ',(this.innerText || this.textContent)); return false;">' . $filestore . $folders . '/' . $file . '</a></td><td>&nbsp;</td>';
+			echo '<td><a class="copylink" href="#" onclick="window.prompt(' . "'Press CTRL+C, then ENTER'" . ',(this.innerText || this.textContent)); return false;">' . $filestore . '/' . $file . '</a>' . $new . '</td><td>&nbsp;</td>';
 		}
-		echo '<td><span class="filesize">' . FilelinksHelper::getFilesize(JPATH_ROOT . '/' . $filestore . $folders . '/' . $file) . '</span></td>';
+		echo '<td><span class="filesize">' . FilelinksHelper::getFilesize(JPATH_ROOT . '/' . $filestore . '/' . $file) . '</span></td>';
 		echo '</tr>';
 	}
 	echo '</tbody></table>';
 
 }
 echo '</div>';
-
-
